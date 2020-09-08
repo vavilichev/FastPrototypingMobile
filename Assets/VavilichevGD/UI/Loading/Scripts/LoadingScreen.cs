6d93f0244af246367edcbf536fdc8d3d23a0f10d
Mad Diamond <maddiamondgames@gmail.com>
2020-09-08 21:52:21 +0300

    UPD: Loading screen new version (0.2)

--- Assets/VavilichevGD/UI/Loading/Scripts/LoadingScreen.cs
+++ Assets/VavilichevGD/UI/Loading/Scripts/LoadingScreen.cs
@@ -1,38 +1,85 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace VavilichevGD.Core.Loadging {
-    public class LoadingScreen : MonoBehaviour {
+    public static class LoadingScreen {
 
-        [SerializeField] protected bool autoHideWhenSceneLoaded = true;
+        #region CONSTANTS
+
+        private const string PREF_PATH = "[LOADING_SCREEN]";
+
+        #endregion
+
         
-        public virtual void Show() {
-            gameObject.SetActive(true);
+        #region DELEGATES
+
+        public delegate void LoadingScreenHandler();
+
+        public static event LoadingScreenHandler OnLoadingScreenShownEvent;
+        public static event LoadingScreenHandler OnLoadingScreenHideStartEvent;
+        public static event LoadingScreenHandler OnLoadingScreenHiddenCompletelyEvent;
+
+        #endregion
+
+
+        public static bool isActive => visual != null && visual.isActive;
+
+        private static LoadingScreenVisualBase visual;
+        
+        
+        private static void CreateVisual() {
+            // Change to custom visual if needed
+            var pref = Resources.Load<LoadingScreenVisualDefault>(PREF_PATH);
+            var createdLoadingScreen = Object.Instantiate(pref);
+            Resources.UnloadUnusedAssets();
+
+            visual = createdLoadingScreen;
+            visual.OnShownEvent += OnShown;
+            visual.OnHideStartEvent += OnHideStart;
+            visual.OnHiddenCompletelyEvent += OnHiddenCompletely;
         }
-
-        public virtual void Hide() {
-            HideInstantly();
+        
+        
+        
+        public static  void Show() {
+            if (visual == null)
+                CreateVisual();
+            
+            visual.Show();
         }
-
-        protected void HideInstantly() {
-            gameObject.SetActive(false);
+        
+        public static void Hide() {
+            if (visual == null)
+                throw new Exception("You cant hide loading screen before creating");
+            
+            visual.Hide();
         }
-
-        protected virtual void OnEnable() {
-            Loading.OnSceneLoadingComplete += OnSceneLoadingComplete;
+        
+        public static void HideInstantly() {
+            if (visual == null)
+                throw new Exception("You cant hide loading screen before creating");
+            
+            visual.HideInstantly();
         }
+        
 
-        protected virtual void OnDisable() {
-            Loading.OnSceneLoadingComplete -= OnSceneLoadingComplete;
-        }
-
-
+        
         #region Events
 
-        private void OnSceneLoadingComplete() {
-            if (autoHideWhenSceneLoaded)
-                Loading.HideLoadingScreen();
+        private static void OnHiddenCompletely(LoadingScreenVisualBase visualbase) {
+            OnLoadingScreenHiddenCompletelyEvent?.Invoke();
+        }
+
+        private static void OnHideStart(LoadingScreenVisualBase visualbase) {
+            OnLoadingScreenHideStartEvent?.Invoke();
+        }
+
+        private static void OnShown(LoadingScreenVisualBase visualbase) {
+            OnLoadingScreenShownEvent?.Invoke();
         }
         
         #endregion
+        
     }
 }
